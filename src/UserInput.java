1fccee8d88e80dcad92783883664292c52e99533
Phong <phongngo91@gmail.com>
2018-09-12 01:04:16 -0400

    initialized userinput field variables, created getter and return

--- src/UserInput.java
+++ src/UserInput.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class UserInput {
+
+    String _firstInput;
+    String _secondInput;
+    String _thirdInput;
+    String _lastString;
+
+    public UserInput(){
+
+    }
+
+    public static String inputGetter() {
+        
+        return thirdOption;
+    }
+
 }
