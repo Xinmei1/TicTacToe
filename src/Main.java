9d7303c47146183e5fa8a3295cce6c5f4fe4f60d
Phong <phongngo91@gmail.com>
2018-09-12 23:02:42 -0400

    Created field variables for grid individual boxes for MyGrid Class

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         UserInput PlayerOneInput = new UserInput();
-        System.out.println(PlayerOneInput.inputGetter());
+
 
     }
 }
--- src/MyGrid.java
+++ src/MyGrid.java
@@ -1,4 +1,15 @@
 public class MyGrid {
 
+    private String _userInput;
+    private String _r1c1 = "_";
+    private String _r1c2 = "_";
+    private String _r1c3 = "_";
+    private String _r2c1 = "_";
+    private String _r2c2 = "_";
+    private String _r2c3 = "_";
+    private String _r3c1 = " ";
+    private String _r3c2 = " ";
+    private String _r3c3 = " ";
+    private String _finalGrid;
 
 }
