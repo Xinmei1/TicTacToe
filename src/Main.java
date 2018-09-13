068a3d6f45479a56448ab9890cb7e5e16d26f246
Phong <phongngo91@gmail.com>
2018-09-12 23:39:05 -0400

    Created a new object from UserInput class, created a new object from the MyGrid Class

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Generates a new object from the MyGrid class
+        
+        MyGrid PlayerGrid = new MyGrid();
         UserInput PlayerOneInput = new UserInput();
+        String inputXO = PlayerOneInput.inputGetter();
+
+        /*Sets grid based on user input
+        More like changes one square of the grid
+        every time the user inputs another String
+         */
+        PlayerGrid.setGrid(inputXO);
+
+
 
 
     }
