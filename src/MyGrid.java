6c64c5f1d697bfc1ab5be9b6238bfbb3b755bfd9
Phong Ngo <phongngo91@gmail.com>
2018-09-13 11:36:24 -0400

    new setgrid alternative variable stable

--- src/MyGrid.java
+++ src/MyGrid.java
@@ -29,42 +29,41 @@ public class MyGrid {
     }
 
     public void setGrid(String playerinput) {
-        _userInput = playerinput;
-        if (this._userInput.equals("X11")) {
+        if (playerinput.equals("X11")) {
             this._r1c1 = "X";
-        } else if (this._userInput.equals("X12")) {
+        } else if (playerinput.equals("X12")) {
             this._r1c2 = "X";
-        } else if (this._userInput.equals("X13")) {
+        } else if (playerinput.equals("X13")) {
             this._r1c3 = "X";
-        } else if (this._userInput.equals("X21")) {
+        } else if (playerinput.equals("X21")) {
             this._r2c1 = "X";
-        } else if (this._userInput.equals("X22")) {
+        } else if (playerinput.equals("X22")) {
             this._r2c2 = "X";
-        } else if (this._userInput.equals("X23")) {
+        } else if (playerinput.equals("X23")) {
             this._r2c3 = "X";
-        } else if (this._userInput.equals("X31")) {
+        } else if (playerinput.equals("X31")) {
             this._r3c1 = "X";
-        } else if (this._userInput.equals("X32")) {
+        } else if (playerinput.equals("X32")) {
             this._r3c2 = "X";
-        } else if (this._userInput.equals("X33")) {
+        } else if (playerinput.equals("X33")) {
             this._r3c3 = "X";
-        } else if (this._userInput.equals("O11")) {
+        } else if (playerinput.equals("O11")) {
             this._r1c1 = "O";
-        } else if (this._userInput.equals("O12")) {
+        } else if (playerinput.equals("O12")) {
             this._r1c2 = "O";
-        } else if (this._userInput.equals("O13")) {
+        } else if (playerinput.equals("O13")) {
             this._r1c3 = "O";
-        } else if (this._userInput.equals("O21")) {
+        } else if (playerinput.equals("O21")) {
             this._r2c1 = "O";
-        } else if (this._userInput.equals("O22")) {
+        } else if (playerinput.equals("O22")) {
             this._r2c2 = "O";
-        } else if (this._userInput.equals("O23")) {
+        } else if (playerinput.equals("O23")) {
             this._r2c3 = "O";
-        } else if (this._userInput.equals("O31")) {
+        } else if (playerinput.equals("O31")) {
             this._r3c1 = "O";
-        } else if (this._userInput.equals("O32")) {
+        } else if (playerinput.equals("O32")) {
             this._r3c2 = "O";
-        } else if (this._userInput.equals("O33")) {
+        } else if (playerinput.equals("O33")) {
             this._r3c3 = "O";
         }
 
