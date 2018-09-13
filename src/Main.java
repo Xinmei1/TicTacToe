7569f5ba3a53ff633457452c8895589c163e0509
Phong <phongngo91@gmail.com>
2018-09-12 23:41:39 -0400

    invoked the method calls of the object PlayerGrid of the class MyGrid  in the Main Class, specifically the printgrid method and return wins method, to print current state of grid and also returns a string telling you the player has won

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         //Generates a new object from the MyGrid class
-        
+
         MyGrid PlayerGrid = new MyGrid();
         UserInput PlayerOneInput = new UserInput();
         String inputXO = PlayerOneInput.inputGetter();
@@ -13,6 +13,10 @@ public class Main {
         every time the user inputs another String
          */
         PlayerGrid.setGrid(inputXO);
+        //print updated grid
+        PlayerGrid.printGrid();
+        //tests if there is 3 in a row or not
+        PlayerGrid.returnsWin();
 
 
 
