c343fe357ccb76095f2ee17262427895467b8a59
Phong <phongngo91@gmail.com>
2018-09-12 23:46:23 -0400

    added loop that checks returnmethod boolean return and prints out a "You win" string

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,26 @@ public class Main {
 
         MyGrid PlayerGrid = new MyGrid();
         UserInput PlayerOneInput = new UserInput();
-        String inputXO = PlayerOneInput.inputGetter();
+
+        for (int i=0; i < 9; i++) {
+
+            String inputXO = PlayerOneInput.inputGetter();
 
         /*Sets grid based on user input
         More like changes one square of the grid
         every time the user inputs another String
          */
-        PlayerGrid.setGrid(inputXO);
-        //print updated grid
-        PlayerGrid.printGrid();
-        //tests if there is 3 in a row or not
-        PlayerGrid.returnsWin();
+            PlayerGrid.setGrid(inputXO);
+            //print updated grid
+            PlayerGrid.printGrid();
+            //tests if there is 3 in a row or not
 
 
+            if (PlayerGrid.returnsWin()){
+                System.out.println("YOU WIN! GOOD JOB");
+                i=9;
+            }
+        }
 
 
     }
