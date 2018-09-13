2ca1af989dd37f476129467c709f25d97f6b9940
Phong <phongngo91@gmail.com>
2018-09-12 23:05:12 -0400

    Created MyGrid constructor, initialized variables inside constructor instead of while declaring field variables

--- src/MyGrid.java
+++ src/MyGrid.java
@@ -1,15 +1,30 @@
 public class MyGrid {
 
     private String _userInput;
-    private String _r1c1 = "_";
-    private String _r1c2 = "_";
-    private String _r1c3 = "_";
-    private String _r2c1 = "_";
-    private String _r2c2 = "_";
-    private String _r2c3 = "_";
-    private String _r3c1 = " ";
-    private String _r3c2 = " ";
-    private String _r3c3 = " ";
+    private String _r1c1 ;
+    private String _r1c2 ;
+    private String _r1c3 ;
+    private String _r2c1 ;
+    private String _r2c2 ;
+    private String _r2c3 ;
+    private String _r3c1 ;
+    private String _r3c2 ;
+    private String _r3c3 ;
     private String _finalGrid;
+
+    public MyGrid(){
+        
+        _r1c1 = "_";
+        _r1c2 = "_";
+        _r1c3 = "_";
+        _r2c1 = "_";
+        _r2c2 = "_";
+        _r2c3 = "_";
+        _r3c1 = " ";
+        _r3c2 = " ";
+        _r3c3 = " ";
+
+
+    }
 
 }
