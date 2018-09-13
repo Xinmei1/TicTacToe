12bc216c1ae4f0d6c6140b150d6ad2e31c5da92f
Phong Ngo <phongngo91@gmail.com>
2018-09-13 15:39:30 -0400

    tried to make a restart loop, not working, fixing

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,27 +10,37 @@ public class Main {
         //Generates a new object from the UserInput class
         UserInput PlayerOneInput = new UserInput();
 
-        //loop for playing TicTacToe
-        int i = 0;
+        String answer = "Y";
 
-        while (i < 9) {
+         while (answer == "Y") {
 
-            //invoke input getter from the input class, returns a string
-            String inputXO = PlayerOneInput.inputGetter();
-            //changes one field variable of MyGrid every time the user inputs another String
-            PlayerGrid.setGrid(inputXO);
+            //loop for playing TicTacToe
+            int i = 0;
 
-            //print updated grid
-            PlayerGrid.printGrid();
+            while (i < 9) {
 
-            //checks if the grid's win condition is fulfilled
-            //tests if there is 3 in a row or not
-            if (PlayerGrid.returnsWin()) {
-                System.out.println("YOU WIN! GOOD JOB");
-                i = 9;
+                //invoke input getter from the input class, returns a string
+                String inputXO = PlayerOneInput.inputGetter();
+                //changes one field variable of MyGrid every time the user inputs another String
+                PlayerGrid.setGrid(inputXO);
+
+                //print updated grid
+                PlayerGrid.printGrid();
+
+                //checks if the grid's win condition is fulfilled
+                //tests if there is 3 in a row or not
+                if (PlayerGrid.returnsWin()) {
+                    System.out.println("YOU WIN! GOOD JOB");
+                    i = 9;
+                }
+
+                i++;
             }
 
-            i++;
+            System.out.println("Do you want to play again? Y or N");
+            Scanner scanner = new Scanner(System.in);
+            answer = scanner.nextLine().toUpperCase();
+
         }
 
     }
