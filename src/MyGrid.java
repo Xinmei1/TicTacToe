3a58997bec003da7a59d3990eecb2679e4add9d6
Phong <phongngo91@gmail.com>
2018-09-12 23:13:09 -0400

    Created a setter for MyGrid class, which sets the square values to X or O based on user input, through the string comparison .equals

--- src/MyGrid.java
+++ src/MyGrid.java
@@ -24,6 +24,47 @@ public class MyGrid {
         _r3c2 = " ";
         _r3c3 = " ";
 
+    }
+
+    public void setGrid(String inputxo) {
+        _userInput = inputxo;
+        if (this._userInput.equals("X11")) {
+            this._r1c1 = "X";
+        } else if (this._userInput.equals("X12")) {
+            this._r1c2 = "X";
+        } else if (this._userInput.equals("X13")) {
+            this._r1c3 = "X";
+        } else if (this._userInput.equals("X21")) {
+            this._r2c1 = "X";
+        } else if (this._userInput.equals("X22")) {
+            this._r2c2 = "X";
+        } else if (this._userInput.equals("X23")) {
+            this._r2c3 = "X";
+        } else if (this._userInput.equals("X31")) {
+            this._r3c1 = "X";
+        } else if (this._userInput.equals("X32")) {
+            this._r3c2 = "X";
+        } else if (this._userInput.equals("X33")) {
+            this._r3c3 = "X";
+        } else if (this._userInput.equals("O11")) {
+            this._r1c1 = "O";
+        } else if (this._userInput.equals("O12")) {
+            this._r1c2 = "O";
+        } else if (this._userInput.equals("O13")) {
+            this._r1c3 = "O";
+        } else if (this._userInput.equals("O21")) {
+            this._r2c1 = "O";
+        } else if (this._userInput.equals("O22")) {
+            this._r2c2 = "O";
+        } else if (this._userInput.equals("O23")) {
+            this._r2c3 = "O";
+        } else if (this._userInput.equals("O31")) {
+            this._r3c1 = "O";
+        } else if (this._userInput.equals("O32")) {
+            this._r3c2 = "O";
+        } else if (this._userInput.equals("O33")) {
+            this._r3c3 = "O";
+        }
 
     }
 
